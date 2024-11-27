6e60a0b5dc54cc993b8dce6b249effd6611c4f23
Falade Yusuf <faladeyusuf54@gmail.com>
2024-11-27 17:49:46 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>COMING SOON!!!</h1>
+      <a href="https://intendslog.com">intendslog.com</a>
+    </div>
+  );
+}
+
+export default App;
